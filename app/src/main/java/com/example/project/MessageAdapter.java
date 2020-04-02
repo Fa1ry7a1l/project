943c4e3d9ea575627cca379914c0613d8590bfca
Fa1ry7a1l <frosty.priense@gmail.com>
2020-04-02 16:02:29 +0300

    Адаптер сообщений

--- app/src/main/java/com/example/project/MessageAdapter.java
+++ app/src/main/java/com/example/project/MessageAdapter.java
@@ -19,14 +19,20 @@ public class MessageAdapter extends ArrayAdapter<Message> {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         final Message message = getItem(position);
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.dialogue_adapter, null);
+        if(message.isMine()) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.my_message, null);
+            }
+            ((TextView) convertView.findViewById(R.id.my_message_body)).setText(message.getMessage());
         }
-
-
-
-
-
+        else
+        {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.other_message, null);
+            }
+            ((TextView) convertView.findViewById(R.id.other_message_body)).setText(message.getMessage());
+           // ((TextView) convertView.findViewById(R.id.sendersName)).setText(message.get);
+        }
 
         return convertView;
     }
