0e41f3c8e6855270220f031c7fd3f61586a5089c
Fa1ry7a1l <frosty.priense@gmail.com>
2020-04-02 07:45:49 +0300

    создал окно чатов, но токстовое поле находится сверху

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -13,26 +13,13 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    Dialogue[] arr={new Dialogue("hi",new ArrayList<Message>()),new Dialogue("hello",new ArrayList<Message>()) };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.chat);
 
-        ListView lv = (ListView) findViewById(R.id.chatList);
 
-        //ArrayAdapter<String> chatAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arr);
-        DialogueAdapter dialogueAdapter=new DialogueAdapter(this,arr);
-        lv.setAdapter(dialogueAdapter);
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                                      @Override
-                                      public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-
-                                         // Toast.makeText(getApplicationContext(),arr[position],Toast.LENGTH_LONG).show();
-                                      }
-                                  }
-        );
 
     }
 
