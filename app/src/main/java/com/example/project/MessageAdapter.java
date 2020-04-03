4c95cb530f1feadd2f5b06524231dd3d9d9a8883
Fa1ry7a1l <frosty.priense@gmail.com>
2020-04-03 10:57:57 +0300

    Исправлено отображение последнего сообщения в чате Исправлены все предупреждения

--- app/src/main/java/com/example/project/MessageAdapter.java
+++ app/src/main/java/com/example/project/MessageAdapter.java
@@ -1,5 +1,6 @@
 package com.example.project;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,37 +14,37 @@ import java.util.ArrayList;
 
 
 public class MessageAdapter extends ArrayAdapter<Message> {
-    public MessageAdapter(Context context, Message[] arr) {
+     MessageAdapter(Context context, Message[] arr) {
         super(context, R.layout.my_message, arr);
     }
-    public MessageAdapter(Context context, ArrayList<Message> messages)
+     MessageAdapter(Context context, ArrayList<Message> messages)
     {
         super(context, R.layout.my_message, messages);
     }
 
+
+   @SuppressLint("InflateParams")
+   @androidx.annotation.NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position,  View convertView, @androidx.annotation.NonNull ViewGroup parent) //override method for displaying chat messages
+       {
 
         final Message message = getItem(position);
-        if(message.isMine()) {
-            //if (convertView == null) {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.my_message, null);
+           assert message != null;
+           if(message.isMine()) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.my_message, parent,false);
 
-            //}
             ((TextView) convertView.findViewById(R.id.my_message_body)).setText(message.getMessage());
         }
         else
         {
             //if (convertView == null) {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.other_message, null);
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.other_message, parent,false);
             //}
-            ((TextView)convertView.findViewById(R.id.sendersName)).setText("Mary");
+            ((TextView)convertView.findViewById(R.id.sendersName)).setText(R.string.default_opponent_name);
             Log.d("MessageAdapter", "Message from other person:"+message.getMessage());
-
+            ((TextView) convertView.findViewById(R.id.other_message_body)).setText(message.getMessage());
         }
-
-
-
         return convertView;
     }
 }
